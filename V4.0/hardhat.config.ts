c710ec9627be4ae5cd49dd822d6fe007132cd4c0
RutujaBalkrishnaGhate <ghate.r@northeastern.edu>
2023-07-12 18:22:58 -0400

    Coverage Report

--- V4.0/hardhat.config.ts
+++ V4.0/hardhat.config.ts
@@ -1,9 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "solidity-coverage"
+import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
 };
 
 export default config;
